46bd6cd4dffc7c7c21efc614dde9c4c73030910f
staseraqui <info@staseraqui.it>
2025-09-05 15:15:19 +0200

    Create app-config.js

---
+++ assets/app-config.js
@@ -0,0 +1,6 @@
+// assets/app-config.js
+// Metti qui i tuoi valori UNA SOLA VOLTA.
+window.SQ_CONFIG = {
+  SUPABASE_URL: 'https://rubdzlnolxcgpjklfggk.supabase.co',
+  SUPABASE_ANON: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1YmR6bG5vbHhjZ3Bqa2xmZ2drIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4OTI3NjQsImV4cCI6MjA3MjQ2ODc2NH0.Fhq7lKdx1OcyuCWOnHqNH7Omsg7qjdQVzxvNqZyeQQ8'
+};
